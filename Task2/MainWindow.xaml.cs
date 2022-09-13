30480f648f5482a23aee8ed2998ba198a8ec8c75
IAmExperimentator <saschak2003@mail.ru>
2022-09-13 13:55:54 +0300

    Добавьте файлы проекта.

---
+++ Task2/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _hashCode;
+        private bool _cancelChanges;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _cancelChanges = false;
+            var user = new User("Dkit", "dkit@test.ru", "DkitGit");
+            _hashCode = user.GetHashCode();
+            DataContext = user;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(DataContext.GetHashCode() != _hashCode && !_cancelChanges)
+            {
+                MDSnackbarUnsavedChanges.IsActive = true;
+                e.Cancel = true;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(DataContext.GetHashCode() != _hashCode && !_cancelChanges)
+            {
+                MDSnackbarUnsavedChanges.IsActive = true;
+                MDSnackbarMessage.Content = "Данные обновлены";
+                MDSnackbarMessage.ActionContent = "OK";
+            }
+        }
+
+        private void SnackbarMessage_ActionClick(object sender, RoutedEventArgs e)
+        {
+            MDSnackbarUnsavedChanges.IsActive = false;
+            _cancelChanges = true;
+            Close();
+        }
+    }
+}
---
+++ Task2/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class User : INotifyPropertyChanged
+    {
+        #region PropertyChangedEventHandler
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanget([CallerMemberName] string name = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+
+        #region Private fields
+        private string _name;
+        private string _email;
+        private string _gitHub;
+        #endregion
+
+        #region Public property
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanget("Name");
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanget("Email");
+            }
+        }
+        
+        public string GitHub
+        {
+            get { return _gitHub; }
+            set
+            {
+                _gitHub = value;
+                OnPropertyChanget("GitHub");
+            }
+        }
+        #endregion
+
+        public User(string name, string email, string gitHub)
+        {
+            _name = name;
+            _email = email;
+            _gitHub = gitHub;
+        }
+
+        public override int GetHashCode()
+        {
+            return (_name + _email + _gitHub).GetHashCode();
+        }
+    }
+}
